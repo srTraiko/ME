648d0757ec22c239f4d50e3955bfbdcd3c18c88c
Traiko <98191053+srTraiko@users.noreply.github.com>
2022-06-24 18:36:09 -0400

    0.2 Cajotas

--- ME.py
+++ ME.py
@@ -1,4 +1,5 @@
 import os
+# from typing import Type
 from colorama import Fore
 
 # base del programa 
@@ -16,104 +17,124 @@ clear()
 bot = Fore.RED + 'Robto 003: ' + '\033[39m'
 
 print(bot + 'Hola mucho gusto, me presento, el dia de hoy sere tu maquina expendedora :D!')
-money = int(input("Ingresa la cantidad de dinero que desees gastar: "))
 
+# Revisar como resolver este error.
 
-clear()
+try:
+    money = int(input(bot + 'Ingresa la cantidad de dinero que desees gastar: '))
 
-def ascii_menu():
-    print(f"""
-        __________________________
-       /                         /|        _____________________
-      /       ^_^ TraiCORP      / |        |   Vista General   |
-     /_________________________/  |        ---------------------
-     | |---------------------| |  |                     {money}
-     | | Maquina Expendedora | |  |         {bebidas}
-     | |---------------------| |  |
-     | | 1. Comprar          | |  |
-     | | 2. vender           | |  |
-     | | 3. Salir            | |  |
-     | |                     | |  |
-     | |                     | |  |
-     | | 99. ???             | | /
-     | |_____________________| |/
-     |_________________________/
-""")
+except:
+    print("error, ese numero no existe")
+else:
 
-def ascii_comprar():
-    print(f"""
-    __________________________
-   /                         /|            ---------------------
-  /       ^_^ TraiCORP      / |            |   Vista General   |
- /_________________________/  |            ---------------------
- | |---------------------| |  |                         {money}
- | | Maquina Expendedora | |  |            {bebidas}
- | |---------------------| |  |
- | | 1. Pepsi         5$ | |  |
- | | 2. Cocacola      5$ | |  |
- | | 3. Regresar         | |  |
- | |                     | |  |
- | |                     | |  |
- | |                     | | /
- | |_____________________| |/
- |_________________________/
- """)
+    print(money)
 
-
-
-while True:
-    ascii_menu()
-    opcion = int(input('Selecciona una opcion: '))
 
     clear()
 
-    # Comprar
-    if opcion == 1:
-        ascii_comprar()
-
-        bebida = int(input("Selecciona una compra: "))
-
-        # Comprar Pepsi
-        if bebida == 1 and money >= 5:
-            bebidas.append('Pepsi')
-            money -= 5
-            print(f"Has comprado una Pepsi, tienes ahora {money}$")
-        # Comprar Cocacola
-        elif bebida == 2 and money >= 5:
-            bebidas.append("Cocacola")
-            money -= 5
-            print(f"Has comprado una Cocacola, tienes ahora {money}$")
-        # Regresar
-        elif bebida == 3:
-            clear()
-        # No existe esa opcion
-        else:
-            print("No existe esa opcion ;(")
-
-    # Vender bebidas
-    if opcion == 2:
-        clear() 
-
+    def ascii_menu():
         print(f"""
-    ---------------------
-    |   Vista General   |
-    ---------------------
-    {bebidas}
+            __________________________
+           /                         /|        _____________________
+          /       ^_^ TraiCORP      / |        |   Vista General   |
+         /_________________________/  |        ---------------------
+         | |---------------------| |  |                     {money}
+         | | Maquina Expendedora | |  |         {bebidas}
+         | |---------------------| |  |
+         | | 1. Comprar          | |  |
+         | | 2. vender           | |  |
+         | | 3. Salir            | |  |
+         | |                     | |  |
+         | |                     | |  |
+         | | 99. ???             | | /
+         | |_____________________| |/
+         |_________________________/
+    """)
 
+    def ascii_comprar():
+        print(f"""
+        __________________________
+       /                         /|            ---------------------
+      /       ^_^ TraiCORP      / |            |   Vista General   |
+     /_________________________/  |            ---------------------
+     | |---------------------| |  |                         {money}
+     | | Maquina Expendedora | |  |            {bebidas}
+     | |---------------------| |  |
+     | | 1. Pepsi         5$ | |  |
+     | | 2. Cocacola      5$ | |  |
+     | | 3. Regresar         | |  |
+     | |                     | |  |
+     | |                     | |  |
+     | |                     | | /
+     | |_____________________| |/
+     |_________________________/
+     """)
 
-""")
+    while True:
 
-        vender = input(Fore.RED + "Escribe la bebida que deseas vender: " + '\033[39m')
-        vender.lower()
+        ascii_menu()
 
-        if vender == 'pepsi':
-            bebidas.remove('Pepsi')
+        try:
+            opcion = int(input('Selecciona una opcion: '))
 
-        elif vender == 'cocacola':
-            bebidas.remove('Cocacola')
-        else:
-            print('No existe esa bebida')
+            clear()
 
-    # Salir
-    if opcion == 3:
-        os._exit(os.EX_OK)
+            # Comprar
+            if opcion == 1:
+                ascii_comprar()
+
+                bebida = int(input("Selecciona una compra: "))
+
+                # Comprar Pepsi
+                if bebida == 1 and money >= 5:
+                    bebidas.append('Pepsi')
+                    money -= 5
+                    print(f"Has comprado una Pepsi, tienes ahora {money}$")
+
+                # Comprar Cocacola
+                elif bebida == 2 and money >= 5:
+                    bebidas.append("Cocacola")
+                    money -= 5
+                    print(f"Has comprado una Cocacola, tienes ahora {money}$")
+
+                # Regresar
+                elif bebida == 3:
+                    clear()
+
+                # No existe esa opcion
+                else:
+                    print("No existe esa opcion ;(")
+        
+            # Vender bebidas
+            if opcion == 2:
+                clear() 
+        
+                print(f"""
+            ---------------------
+            |   Vista General   |
+            ---------------------
+            {bebidas}
+        
+        
+        """)
+        
+                vender = input(Fore.RED + "Escribe la bebida que deseas vender: " + '\033[39m')
+        
+                if vender == 'pepsi':
+                    vender.lower()
+                    bebidas.remove('Pepsi')
+        
+                elif vender == 'cocacola':
+                    vender.lower()
+                    bebidas.remove('Cocacola')
+                else:
+                    print('No existe esa bebida')
+        
+            # Salir
+            if opcion == 3:
+                os._exit(os.EX_OK)
+        
+        except ValueError:
+            print("Error, ese numero no es valido")
+            break
+        
